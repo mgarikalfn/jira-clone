7e5221e739cdccf184a9839ba87bc1be44ab7721
girum <girumkenenisa@gmail.com>
2025-09-13 23:25:47 +0300

    fixed the calendar component

--- components/ui/date-picker.tsx
+++ components/ui/date-picker.tsx
@@ -2,10 +2,10 @@
 
 import * as React from "react";
 import {format} from "date-fns";
-import {Calendar} from "@/components/ui/calendar";
+import { Calendar as CalendarIcon } from "lucide-react"; 
+import { Calendar } from "@/components/ui/calendar";   
 import {Popover,PopoverContent,PopoverTrigger} from "@/components/ui/popover" ;
 import { Button } from "./button";
-import { CalendarIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 interface DatePickerProps{
@@ -17,29 +17,30 @@ interface DatePickerProps{
 
 export const DatePicker = ({value ,onChange,className,placeholder="select date"}:DatePickerProps) =>{
   return (
-    <Popover>
-        <PopoverTrigger asChild>
-            <Button
-                variant="outline"
-                size="lg"
-                className={cn(
-                    "w-full justify-start  text-left font-normal px-3",
-                    !value && "text-muted-foreground",
-                    className
-                )}
-            >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-            </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0">
-            <Calendar
-              mode="single"
-              selected={value}
-              onSelect={(data) => onChange(data as Date)}
-              initialFocus
-              />
-              
-        </PopoverContent>
-    </Popover>
+   <Popover>
+  <PopoverTrigger asChild>
+    <Button
+      variant="outline"
+      size="lg"
+      className={cn(
+        "w-full justify-start text-left font-normal px-3",
+        !value && "text-muted-foreground",
+        className
+      )}
+    >
+      <CalendarIcon className="mr-2 h-4 w-4" />
+      {value ? format(value, "PPP") : <span>{placeholder}</span>}
+    </Button>
+  </PopoverTrigger>
+  <PopoverContent className="w-auto p-0">
+    <Calendar
+      mode="single"
+      selected={value}
+      onSelect={(date) => onChange(date as Date)}
+      autoFocus
+    />
+  </PopoverContent>
+</Popover>
+
   )
 }
