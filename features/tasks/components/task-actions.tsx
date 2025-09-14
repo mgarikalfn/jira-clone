038d8c363f2ac096a694e6cc3b793441149ce201
girum <girumkenenisa@gmail.com>
2025-09-14 12:24:42 +0300

    created task delete and update fuctionality

--- features/tasks/components/task-actions.tsx
+++ features/tasks/components/task-actions.tsx
@@ -1,5 +1,9 @@
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useConfirm } from "@/hooks/use-confirm";
 import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
+import { useDeleteTask } from "../api/use-delete-task";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 interface TaskActionsProps{
     id:string;
@@ -8,24 +12,52 @@ interface TaskActionsProps{
 }
 
 export const TaskActions = ({id,projectId,children}:TaskActionsProps) =>{
+    const workspaceId = useWorkspaceId();
+    const router = useRouter();
+
+
+    const [ConfirmDialog , confirm] = useConfirm(
+        "Delete task",
+        "This action cannot be undone",
+        "destructive"
+    );
+
+    const {mutate , isPending} = useDeleteTask();
+
+    const onDelete = async () => {
+        const ok = await confirm();
+        if(!ok) return ;
+
+        mutate({param:{taskId : id}})
+    };
+
+    const onOpenTask = () => {
+        router.push(`/workspaces/${workspaceId}/tasks/${id}`)
+    }
+
+    const onOpenProject = () => {
+        router.push(`/workspaces/${workspaceId}/projects/${projectId}`)
+    }
+
     return (
         <div className="flex justify-end">
+            <ConfirmDialog/>
             <DropdownMenu modal={false}>
                 <DropdownMenuTrigger asChild>
                     {children}
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end" className="w-48">
                     <DropdownMenuItem 
-                        onClick={() => {}}
-                        disabled={false}
+                        onClick={onOpenTask}
+                        
                         className="font-medium p-[10px]"
                     >
                         <ExternalLinkIcon className="size-4 mr-2 stroke-2"/>
                         Task Details
                     </DropdownMenuItem>
                     <DropdownMenuItem 
-                        onClick={() => {}}
-                        disabled={false}
+                        onClick={onOpenProject}
+                       
                         className="font-medium p-[10px]"
                     >
                         <ExternalLinkIcon className="size-4 mr-2 stroke-2"/>
@@ -33,15 +65,15 @@ export const TaskActions = ({id,projectId,children}:TaskActionsProps) =>{
                     </DropdownMenuItem>
                     <DropdownMenuItem 
                         onClick={() => {}}
-                        disabled={false}
+                        
                         className="font-medium p-[10px]"
                     >
                         <PencilIcon className="size-4 mr-2 stroke-2"/>
                         Edit Task 
                     </DropdownMenuItem>
                     <DropdownMenuItem 
-                        onClick={() => {}}
-                        disabled={false}
+                        onClick={onDelete}
+                        disabled={isPending}
                         className="text-amber-700 focus:text-amber-700 font-medium p-[10px]"
                     >
                         <TrashIcon className="size-4 mr-2 stroke-2"/>
--- features/tasks/server/route.ts
+++ features/tasks/server/route.ts
@@ -185,5 +185,146 @@ const app = new Hono()
     
     }
 )
+.patch(
+    '/:taskId',
+    sessionMiddleware,
+    zValidator("json",createTaskSchema.partial()),
+    async(c) => {
+        const user = c.get("user");
+        const databases = c.get("databases");
+        const {
+            name,
+            status,
+            description,
+            projectId,
+            dueDate,
+            assigneeId
+        } = c.req.valid("json");
+
+        const {taskId} = c.req.param();
+
+        const existingTask = await databases.getDocument<Task>(
+            DATABASE_ID,
+            TASKS_ID,
+            taskId
+        )
+
+        const member = await getMember({ 
+            databases,
+            workspaceId:existingTask.workspaceId,
+            userId:user.$id
+    });
+
+    if(!member){
+        return c.json({error:"unauthorized"} , 401)
+    }
+
+    
+    const task =await databases.updateDocument<Task>(
+        DATABASE_ID,
+        TASKS_ID,
+        taskId,
+        {
+            name,
+            status,
+            projectId,
+            dueDate,
+            assigneeId,
+            description
+        },
+    )
+
+    return c.json({data:task});
+    
+    }
+)
+.post(
+    '/',
+    sessionMiddleware,
+    zValidator("json",createTaskSchema),
+    async(c) => {
+        const user = c.get("user");
+        const databases = c.get("databases");
+        const {
+            name,
+            status,
+            workspaceId,
+            projectId,
+            dueDate,
+            assigneeId
+        } = c.req.valid("json");
+
+        const member = await getMember({ 
+            databases,
+            workspaceId,
+            userId:user.$id
+    });
+
+    if(!member){
+        return c.json({error:"unauthorized"} , 401)
+    }
+
+    const highestPositionTask = await databases.listDocuments(
+        DATABASE_ID,
+        TASKS_ID,
+        [
+            Query.equal("status" , status),
+            Query.equal("workspaceId",workspaceId),
+            Query.orderAsc("position"),
+            Query.limit(1),
+        ]
+    );
+
+    const newPosition = highestPositionTask.documents.length > 0
+    ?highestPositionTask.documents[0].position + 1000
+    :1000;
+
+    const task =await databases.createDocument(
+        DATABASE_ID,
+        TASKS_ID,
+        ID.unique(),
+        {
+            name,status,workspaceId,projectId,dueDate,assigneeId,position:newPosition
+        }
+    )
+
+    return c.json({data:task});
+    
+    }
+)
+
+.delete(
+    "/:taskId",
+    sessionMiddleware,
+    async (c) => {
+        const user = c.get("user");
+        const databases = c.get("databases");
+        const {taskId} = c.req.param();
+
+        const task = await databases.getDocument<Task>(
+            DATABASE_ID,
+            TASKS_ID,
+            taskId,
+        )
+
+        const member = await getMember({
+            databases,
+            workspaceId:task.workspaceId,
+            userId:user.$id,
+        })
+
+        if(!member){
+            return c.json({error:"unauthorized"},401);
+        }
+
+        await databases.deleteDocument(
+            DATABASE_ID,
+            TASKS_ID,
+            taskId,
+        )
+
+        return c.json({data:{$id:task.$id}})
+    }
+)
 
 export default app;
