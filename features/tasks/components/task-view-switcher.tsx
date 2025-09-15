45da1029e14ffb89c979d7e97fbf0ee454ed92f8
girum <girumkenenisa@gmail.com>
2025-09-15 22:24:33 +0300

    finished the calendar task view components

--- features/tasks/components/task-view-switcher.tsx
+++ features/tasks/components/task-view-switcher.tsx
@@ -17,6 +17,7 @@ import { DataKanban } from "./data-kanban";
 import { useCallback } from "react";
 import { TaskStatus } from "../types";
 import { useBulkUpdateTasks } from "../api/use-bulk-update-task";
+import { DataCalendar } from "./data-calendar";
 
 export const TaskViewSwitcher = () => {
   const [{ status, assigneeId, projectId, dueDate }, setFilters] =
@@ -89,8 +90,8 @@ export const TaskViewSwitcher = () => {
             <TabsContent value="kanban" className="mt-0">
               <DataKanban onChange={onKanbanChange} data={tasks?.documents ?? []} />
             </TabsContent>
-            <TabsContent value="calendar" className="mt-0">
-              {JSON.stringify(tasks)}
+            <TabsContent value="calendar" className="mt-0 h-full pb-4">
+              <DataCalendar data={tasks?.documents ?? []} />
             </TabsContent>
           </>
         )}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+     "./app/**/*.css",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#FF5733',
+      },
+    },
+  },
+  plugins: [],
+};
