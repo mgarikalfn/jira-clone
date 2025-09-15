5e1e61539018371b482d73cf6f1518d53efb47b5
girum <girumkenenisa@gmail.com>
2025-09-15 23:24:42 +0300

    delete functionality from the task details page

---
+++ app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { PageError } from "@/components/page-error";
+import { PageLoader } from "@/components/page-loader";
+
+import { useGetTask } from "@/features/tasks/api/use-get-task";
+import { TaskBreadCrumbs } from "@/features/tasks/components/task-breadcrumbs";
+import { useTaskId } from "@/features/tasks/hooks/use-task-id"
+
+export const TaskIdClient = async() => {
+    const taskId = useTaskId();
+    const {data , isLoading} = useGetTask({taskId});
+
+    if(isLoading){
+        return <PageLoader />
+    }
+
+    if(!data){
+        return <PageError message="Task not found" />
+    }
+   return (
+    <div className="flex flex-col">
+        <TaskBreadCrumbs project={data.project} task={data}/>
+    </div>
+   )
+}
