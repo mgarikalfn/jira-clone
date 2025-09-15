8c38f2d9a28190f959448ebc9dced79a686e31fb
girum <girumkenenisa@gmail.com>
2025-09-16 00:12:55 +0300

    finished the task page and added task description

--- app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,9 +1,12 @@
 "use client"
 import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
+import { DottedSeparator } from "@/components/ui/dotted-separator";
 
 import { useGetTask } from "@/features/tasks/api/use-get-task";
 import { TaskBreadCrumbs } from "@/features/tasks/components/task-breadcrumbs";
+import { TaskDescription } from "@/features/tasks/components/task-description";
+import { TaskOverview } from "@/features/tasks/components/task-overview";
 import { useTaskId } from "@/features/tasks/hooks/use-task-id"
 
 export const TaskIdClient = async() => {
@@ -20,6 +23,11 @@ export const TaskIdClient = async() => {
    return (
     <div className="flex flex-col">
         <TaskBreadCrumbs project={data.project} task={data}/>
+        <DottedSeparator className="my-6"/>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <TaskOverview task={data}/>
+            <TaskDescription task={data} />
+        </div>
     </div>
    )
 }
